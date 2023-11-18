47b2ca34106ad358db63bee57064f92cb7236180
Pratchaya Vongjongrak <pratchaya.vongjongrak@nanameue.jp>
2023-11-19 01:52:09 +0900

    feat: sample db

---
+++ src/server.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+const jsonServer = require('json-server')
+const db = require('./db.json')
+
+const server = jsonServer.create()
+const router = jsonServer.router(db)
+const middlewares = jsonServer.defaults()
+
+const PORT = process.env.PORT || 3002
+const DELAY = 300
+
+server.use(middlewares)
+
+server.use((req, res, next) => {
+  setTimeout(() => {
+    next()
+  }, DELAY)
+})
+
+server.use(router)
+
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on http://localhost:${PORT}`)
+})
